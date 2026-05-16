6342fa98779a9e38bd65d5e8c2bfe8659e66c67e
Enkhbaysgalan <70845310+Enkhbaysgalan@users.noreply.github.com>
2026-05-16 15:17:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "primary",
+  themeColor: "#00DF81",
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
